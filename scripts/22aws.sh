cd1ec41a8212cddf562ad78659dc9cbae8cfe06a
kief <kmorris@thoughtworks.com>
2016-05-25 14:57:29 +0100

    Added AWS config file

--- scripts/22aws.sh
+++ scripts/22aws.sh
@@ -1,2 +1,12 @@
 
 apt-get install -y awscli
+
+mkdir -p -m 0700 /home/vagrant/.aws
+
+cat > /home/vagrant/.aws <<ENDAWSCONFIG
+
+[default]
+region = eu-west-1
+
+ENDAWSCONFIG
+chown -R vagrant:vagrant /home/vagrant/.aws
