4716b58c38e02a2aa1993b1056f4cd3bf734e579
kief <kmorris@thoughtworks.com>
2016-05-26 11:32:59 +0100

    Renamed the packer template to make it not OS/distro/version specific

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ fail()
   exit 1
 }
 
-packer build -color=true $* ubuntu-14.04.3-x86_64.json || fail "Packer failed"
+packer build -color=true $* infra-workbox-template.json || fail "Packer failed"
 vagrant box add -f --name infra-workbox infra-workbox.box || fail "Failed to add the new box"
 
