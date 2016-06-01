378c0578cca931581fedbef13142b52a88ce267a
kief <kmorris@thoughtworks.com>
2016-06-01 15:20:57 +0100

    Install bundler

--- packer-scripts/10ruby.sh
+++ packer-scripts/10ruby.sh
@@ -4,4 +4,4 @@ apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get update
 apt-get install -y ruby2.2 ruby2.2-dev liblzma-dev zlib1g-dev
 echo "gem: --no-document" > /etc/gemrc
-
+gem install bundler
