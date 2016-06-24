e82d140e1947f139e78dbf2f25e8b8fbf9ab9cee
kief <kmorris@thoughtworks.com>
2016-06-24 17:21:29 +0100

    Cleaned out unneeded files

--- build.sh
+++ build.sh
@@ -10,6 +10,20 @@ if [ -f ../.secrets/packer-secrets.sh ] ; then
   . ../.secrets/packer-secrets.sh
 fi
 
-packer build -color=true $* infra-workbox-template.json || fail "Packer failed"
-vagrant box add -f --name infra-workbox infra-workbox.box || fail "Failed to add the new box"
+case $1 in
+  wiley|15*)
+    NAME=infra-workbox-wiley
+    ;;
+  trusty|14*)
+    NAME=infra-workbox
+    ;;
+  *)
+    echo "Add the Ubuntu version on the command line: 14, 15, or 16"
+    exit
+    ;;
+esac
+shift
+
+packer build -only=${NAME} -color=true $* multi-workbox-template.json || fail "Packer failed"
+vagrant box add -f --name "${NAME}" "${NAME}".box || fail "Failed to add the new box"
 
--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 vagrant halt
-rm -rf ./infra-workbox.box ./output-virtualbox-iso ./packer_cache ./.vagrant/machines
+rm -rf ./*.box ./output-*-iso ./packer_cache ./.vagrant/machines
 
