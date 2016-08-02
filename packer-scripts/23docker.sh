71f8e614c9a64bfc62ce3d55f0999e9eb45a87eb
kief <kmorris@thoughtworks.com>
2016-08-02 16:19:03 +0100

    Fix username for docker group

--- packer-scripts/23docker.sh
+++ packer-scripts/23docker.sh
@@ -5,7 +5,7 @@ apt-get install -y linux-image-extra-$(uname -r) apparmor apt-transport-https ca
 apt-get purge lxc-docker
 # apt-cache policy docker-engine
 apt-get install -y docker.io
-usermod -aG docker ubuntu
+usermod -aG docker vagrant
 sed -i -e 's/^GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
 update-grub
 
