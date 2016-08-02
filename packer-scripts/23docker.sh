20c777d7f2976aa60bad7f8ff115e5d941c1f657
kief <kmorris@thoughtworks.com>
2016-08-02 16:04:03 +0100

    Allow vagrant user to run docker without sudo

--- packer-scripts/23docker.sh
+++ packer-scripts/23docker.sh
@@ -5,3 +5,4 @@ apt-get install -y linux-image-extra-$(uname -r) apparmor apt-transport-https ca
 apt-get purge lxc-docker
 # apt-cache policy docker-engine
 apt-get install -y docker.io
+usermod -aG docker ubuntu
