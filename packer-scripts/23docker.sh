702c05b368778f8f416fc3f92235663761000ac2
kief <kmorris@thoughtworks.com>
2016-08-03 10:54:53 +0100

    Switch docker package from docker.io to docker-engine. Seems to be much newer version

--- packer-scripts/23docker.sh
+++ packer-scripts/23docker.sh
@@ -3,8 +3,7 @@ set -x
 apt-get install -y linux-image-extra-$(uname -r) apparmor apt-transport-https ca-certificates
 
 apt-get purge lxc-docker
-# apt-cache policy docker-engine
-apt-get install -y docker.io
+apt-get install -y docker-engine
 usermod -aG docker vagrant
 sed -i -e 's/^GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
 update-grub
