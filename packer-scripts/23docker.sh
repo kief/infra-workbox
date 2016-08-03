b313360d258dcbd9b2ecc44e7d28a662f02590ab
kief <kmorris@thoughtworks.com>
2016-08-03 10:30:12 +0100

    Install docker-compose

--- packer-scripts/23docker.sh
+++ packer-scripts/23docker.sh
@@ -9,3 +9,4 @@ usermod -aG docker vagrant
 sed -i -e 's/^GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
 update-grub
 
+pip install docker-compose
