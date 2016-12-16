e00c1a698c67be4fa4112e50f1b875ad2d3f5751
kief <kmorris@thoughtworks.com>
2016-12-16 08:50:46 +0000

    Remove ssh key config

--- packer-scripts/31vagrant-ssh.sh
+++ packer-scripts/31vagrant-ssh.sh
@@ -1,10 +1,3 @@
 
 mkdir -p -m 0700 /home/vagrant/.ssh
-
-cat > /home/vagrant/.ssh/config <<ENDSSHCONFIG
-Host *
-    User ubuntu
-    IdentityFile ~/.ssh/infraworkbox_key
-ENDSSHCONFIG
-
 chown -R vagrant:vagrant /home/vagrant/.ssh
