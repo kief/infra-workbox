d7844229b34a59ff6d8bac3d75bd7b4c5fe05c45
kief <kmorris@thoughtworks.com>
2018-01-16 11:31:27 +0000

    Revised the terraform and packer install scripts to put the binaries directly into /usrlocal/bin.

--- packer-scripts/71terraform.sh
+++ packer-scripts/71terraform.sh
@@ -1,28 +1,17 @@
-set -x
+set -ex
 
 TERRAFORM_VERSION=0.11.2
 
-fail()
-{
-  echo "FAIL: $*"
-  exit 1
-}
+if [ ! -e /usr/local/bin/terraform ] ; then
+  apt-get install -y unzip
 
-if [ ! -d /usr/local/terraform ] ; then
-  apt-get install -y unzip || fail "Can't install unzip"
+  mkdir -p /tmp/terraform-download
+  cd /tmp/terraform-download
 
-  [ -d /tmp/terraform ] || mkdir /tmp/terraform
-  cd /tmp/terraform
-
-  curl -Os https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip || fail "Can't download terraform"
-  unzip -q terraform_${TERRAFORM_VERSION}_linux_amd64.zip || fail "Can't unzip terraform"
-  rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-  cd /tmp
-  mv terraform /usr/local/ || fail "Can't move terraform into place"
-
-  if [ ! -f /etc/profile.d/terraform.sh ] ; then
-    echo "export PATH=\"\${PATH}:/usr/local/terraform\"" > /etc/profile.d/terraform.sh
-  fi
+  curl -Os https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+  unzip -q terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+  rm ./terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+  mv ./terraform /usr/local/bin/
 else
-  echo "terraform directory already exists in /usr/local/terraform"
+  echo "terraform binary is already installed"
 fi
--- packer-scripts/72packer.sh
+++ packer-scripts/72packer.sh
@@ -1,28 +1,17 @@
-set -x
+set -ex
 
 PACKER_VERSION=1.1.3
 
-fail()
-{
-  echo "FAIL: $*"
-  exit 1
-}
+if [ ! -e /usr/local/bin/packer ] ; then
+  apt-get install -y unzip
 
-if [ ! -e /usr/local/packer ] ; then
-  apt-get install -y unzip || fail "Can't install unzip"
+  mkdir -p /tmp/packer-download
+  cd /tmp/packer-download
 
-  [ -d /tmp/packer ] || mkdir /tmp/packer
-  cd /tmp/packer
-
-  curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip || fail "Can't download packer"
-  unzip -q packer_${PACKER_VERSION}_linux_amd64.zip || fail "Can't unzip packer"
-  rm packer_${PACKER_VERSION}_linux_amd64.zip
-  cd /tmp
-  mv packer /usr/local/ || fail "Can't move packer into place"
-
-  if [ ! -f /etc/profile.d/packer.sh ] ; then
-    echo "export PATH=\"\${PATH}:/usr/local/packer\"" > /etc/profile.d/packer.sh
-  fi
+  curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+  unzip -q packer_${PACKER_VERSION}_linux_amd64.zip
+  rm ./packer_${PACKER_VERSION}_linux_amd64.zip
+  mv ./packer /usr/local/bin/
 else
-  echo "packer directory already exists in /usr/local/packer"
+  echo "packer binary is already installed"
 fi
