4d24f205946cd9dd0937988975501c217c8becd2
kief <kmorris@thoughtworks.com>
2018-04-12 13:41:44 +0100

    Added kops

--- packer-scripts/62kubernetes.sh
+++ packer-scripts/62kubernetes.sh
@@ -1,3 +1,11 @@
 snap install kubectl --classic
 snap install minikube
 snap install helm
+
+if [ ! -e /usr/local/bin/kops ] ; then
+  mkdir -p /tmp/kops-download
+  cd /tmp/kops-download
+  curl -LfOs https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+  chmod +x kops-linux-amd64
+  mv kops-linux-amd64 /usr/local/bin/kops
+fi
