41101a57f3f4b1fb69e99a4ffacf3d10c6fe7f3a
kief <kmorris@thoughtworks.com>
2018-04-19 15:01:42 +0100

    Add some shell aliases I like

---
+++ packer-scripts/91profile.sh
@@ -0,0 +1,6 @@
+
+echo "alias diff='diff -U 0 -b -w'"    > /etc/profile.d/misc.sh
+echo "alias ll='ls -l'"   >> /etc/profile.d/misc.sh
+echo "alias la='ls -alF'" >> /etc/profile.d/misc.sh
+echo "alias ls='ls -F'"   >> /etc/profile.d/misc.sh
+echo "alias l='ls -CF'"   >> /etc/profile.d/misc.sh
